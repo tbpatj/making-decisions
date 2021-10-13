418695302a6eb648859f9af16561caf38e9280ec
Timothy Luebke <luebketj@gmail.com>
2021-10-13 14:30:06 -0600

    finished problem 8

--- lab.js
+++ lab.js
@@ -135,7 +135,10 @@ let passingScore = 7
 */
 
 //CODE HERE
-
+while(score < passingScore){
+  console.log("Your score is not high enough");
+  score ++;
+}
 ////////// INTERMEDIATE PROBLEMS //////////
 
 ////////// PROBLEM 9 //////////
